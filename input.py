314efa8eeca81d2aa8f7d189baa5e13f8e7ba923
askashahira <166271149+askashahira@users.noreply.github.com>
2024-10-20 23:22:57 +0700

    Update input.py

--- input.py
+++ input.py
@@ -2,3 +2,7 @@ def konversi(user_input):
     user_input = user_input.replace("^", "**").replace("x", "*x")
     final = user_input.lstrip("*")
     return final
+
+def eval_funct(user_input, x):
+    func = konversi(user_input)
+    try:
