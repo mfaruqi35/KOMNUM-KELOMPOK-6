ecddad315b7531861158ab16bbdd236900b83bbe
akrimahusri <167009049+akrimahusri@users.noreply.github.com>
2024-10-21 12:57:03 +0700

    Update errorMasssage.py

--- errorMasssage.py
+++ errorMasssage.py
@@ -7,3 +7,13 @@ def validate_input(fungsi, batas_bawah, batas_atas, toleransi_error, iterasi):
         batas_atas = float(batas_atas)
         toleransi_error = float(toleransi_error)
         iterasi = int(iterasi)
+
+     if batas_bawah >= batas_atas:
+            raise ValueError("Batas bawah harus lebih kecil dari batas atas!")
+        
+        if toleransi_error <= 0:
+            raise ValueError("Toleransi error harus lebih besar dari 0!")
+        
+        if iterasi <= 0:
+            raise ValueError("Jumlah iterasi harus lebih besar dari 0!")
+
