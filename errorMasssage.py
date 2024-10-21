424c125555ecbd18e1038406434840d4a5c733a6
akrimahusri <167009049+akrimahusri@users.noreply.github.com>
2024-10-21 15:22:27 +0700

    Update errorMasssage.py

--- errorMasssage.py
+++ errorMasssage.py
@@ -1,5 +1,6 @@
 from tkinter import messagebox
 
+# Fungsi untuk memvalidasi input yang diberikan
 def validate_input(fungsi, batas_bawah, batas_atas, toleransi_error, iterasi):
     try:
         # Periksa apakah input yang diterima valid
@@ -8,22 +9,23 @@ def validate_input(fungsi, batas_bawah, batas_atas, toleransi_error, iterasi):
         toleransi_error = float(toleransi_error)
         iterasi = int(iterasi)
 
+     # Memeriksa apakah batas_bawah lebih kecil dari batas_atas
      if batas_bawah >= batas_atas:
             raise ValueError("Batas bawah harus lebih kecil dari batas atas!")
-        
-        if toleransi_error <= 0:
+
+     # Memeriksa apakah toleransi_error lebih besar dari 0
+     if toleransi_error <= 0:
             raise ValueError("Toleransi error harus lebih besar dari 0!")
-        
-        if iterasi <= 0:
+
+     # Memeriksa apakah jumlah iterasi lebih besar dari 0
+     if iterasi <= 0:
             raise ValueError("Jumlah iterasi harus lebih besar dari 0!")
 
-         # Jika semua validasi berhasil
+     # Jika semua validasi berhasil
             return True
-        except ValueError as e:
-            # Jika ada kesalahan, tampilkan pesan error
+         
+     # Menangani kesalahan input
+     except ValueError as e:
+            # Jika ada kesalahan, tampilkan pesan error menggunakan messagebox
             messagebox.showerror("Input Error", str(e))
             return False
-
-
-        
-
