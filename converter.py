f0e84b06e7d1b318f0009519d628cf0a89e6a395
askashahira <166271149+askashahira@users.noreply.github.com>
2024-10-22 11:53:51 +0700

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,8 +1,12 @@
 import re
+import math
+
 # fungsi konversi
 def convert_function(func):
     # Mengubah tanda '^' menjadi '**' untuk eksponen
     func = func.replace('^', '**')  
     # Mengubah angka dan variabel menjadi format perkalian (misal 2x -> 2*x)
     func = re.sub(r"(\d)(x)", r"\1*x", func)  
-    return func
+
+    final_func = func.lstrip("*")
+    return final_func
